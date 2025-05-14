c3456b4d619eb6d65bc3739ca603289f43c2e74c
Choi Eunsu <zxzx320@naver.com>
2025-05-14 17:23:13 +0900

    0514, 변수

--- HelloWorld/src/com/yedam/JSExe.java
+++ HelloWorld/src/com/yedam/JSExe.java
@@ -1,25 +1,27 @@
 package com.yedam;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class JSExe {
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		test2();
-=======
-
-		// 변경된 부분.
-		// 아무내용이나 ...
+		test4();
 		
-		// 임의의 수를 생성 1 ~ 100사이의 임의의 값을 생성.
-		Math.random(); // 0 <= x < 1 | Math.random() * 10 : 0 <= x < 10
-		int sum = 0;
-		for (int i = 1; i <= 5; i++) {
-			int result = (int) (Math.random() * 100) + 1; // 1 <= x < 101			
-			sum += result;
-		} // for
-		System.out.println(sum);
->>>>>>> branch 'master' of https://github.com/eunsuu0320/HelloWorld.git
+//		int num1 = 30;
+//		int num2 = 30;
+//		
+//		System.out.println(num1 == num2);
+//		
+//		String str1 = new String("Hello");
+//		String str2 = new String("Hello");
+//		
+//		System.out.println(str1);
+//		System.out.println(str2);
+//		
+//		System.out.println(str1.equals(str2));
 	} // main
 
+	
 	public static void test() {
 		int sum = 0;
 		// 1부터 10까지의 값을 누적하는 반복문
@@ -31,14 +33,61 @@ public class JSExe {
 		System.out.println(sum);
 	} // test
 
+	
 	public static void test2() {
-		// 임의의 수를 생성 1 ~ 100사이의 임의의 값을 생성.
+		// 임의의 수를 생성 30 ~ 100사이의 임의의 값을 생성.
+		// 평균: 173/5 => 34.6
 		Math.random(); // 0 <= x < 1 | Math.random() * 10 : 0 <= x < 10
+
 		int sum = 0;
-		for (int i = 1; i <= 5; i++) {
-			int result = (int) (Math.random() * 100) + 1; // 1 <= x < 101
+		for (int i = 1; i <= 7; i++) {
+			double result =(int) (Math.random() * 71) + 30; // 1 <= x < 101
 			sum += result;
 		} // for
-		System.out.println(sum);
+		double avg = sum / 7.0;
+		System.out.println("합 : " + sum);
+		System.out.println("평균 : " + avg);
+		System.out.println(Math.round(avg * 100) / 100.0); //Math.round 소숫점 반올림
+		System.out.println(String.format("%.3f", avg));    //String.format %.nf n만큼 소숫점을 나타내겠다.
+		
+		int n = 30;
+		String s = "30";
+		System.out.println(Integer.parseInt(s) == n); // 문자열을 숫자로 변환해서 비교
 	} // test2
+	
+	
+	public static void test3() {
+	// 사용자의 입력값을 읽어들이기.
+		Scanner scn = new Scanner(System.in);
+		
+		int sum = 0;
+		for (int i = 1; i <= 3; i++) {
+			System.out.println("학생의 점수를 입력하세요.");
+			String value = scn.nextLine(); // 입력값을 문자열형태로 변환.
+			int score = Integer.parseInt(value);
+			sum += score;
+		} // for
+		double avg = sum / 3.0;
+		System.out.println("합 : " + sum + ", 평균 : " + avg);
+		System.out.println("평균 : " + Math.round(avg * 100) / 100.0); //Math.round 소숫점 반올림
+		System.out.println("평균 : " + String.format("%.3f", avg));   
+	} // test3
+	
+	
+	public static void test4 () {
+		// 사용자에게서 받아온 이름들을 "친구 목록은 누구누구, 누구누구, 누구누구 입니다." 로 출력.
+		Scanner scn = new Scanner(System.in);
+		String name = "친구 목록은 ";
+		
+		while(true) {
+			System.out.println("친구의 이름을 입력하세요. 종료 하려면 quit 를 입력하세요.");
+			String msg = scn.nextLine();			
+			if (msg.equals("quit")) {
+				break;
+			} // if
+			name += msg + " ";
+		} // while
+		System.out.println(name + "입니다.");
+		System.out.println("프로그램이 종료되었습니다.");
+	} // test4
 }
