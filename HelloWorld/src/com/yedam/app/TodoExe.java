4fabf00aced583ecba00195e920f56071c2de1c0
choi eunsu <zxzx320@naver.com>
2025-05-19 23:21:45 +0900

    숙제 완성 수정

--- HelloWorld/src/com/yedam/app/TodoExe.java
+++ HelloWorld/src/com/yedam/app/TodoExe.java
@@ -3,18 +3,15 @@ package com.yedam.app;
 public class TodoExe {
 	public static void main(String[] args) {
 		int[][] numAry = new int[3][5];
-		int sum = 0;
-		int count = 0;
-		double avg = 0;
 		// 임의의 숫자 생성.
 		for (int outer = 0; outer < numAry.length; outer++) {
+			int sum = 0;
+			double avg = 0;
 			for (int inner = 0; inner < numAry[outer].length; inner++) {
 				numAry[outer][inner] = (int) (Math.random() * 100);
 				sum += numAry[outer][inner];
-				count++;
 			}
-			
-			avg = 1.0 * sum / count;
+			avg = 1.0 * sum / numAry[outer].length;
 			System.out.printf("numAry[%d]의 평균값은 %.2f\n", outer, avg);
 		}
 	}
